e9be6d004ecdef0e9de5da68447dc4031d95f52d
Mr.Blacky <mr.blacky01@gmail.com>
2017-06-08 15:52:11 +0300

    Change class store file as base64string

---
+++ root/Source/ServiceOrder.DataProvider/Migrations/201706080632171_SaveFileInBase64.cs
@@ -0,0 +1,19 @@
+namespace ServiceOrder.DataProvider.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SaveFileInBase64 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Photos", "PhotoImage");
+            AddColumn("dbo.Photos", "PhotoImage", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Photos", "PhotoImage", c => c.Binary(nullable: false, storeType: "image"));
+        }
+    }
+}
